a3058c69ae4110825542cb7ca55e2396974df7f1
artilluminati <artilluminatigd@gmail.com>
2024-07-24 22:09:04 +0300

    Added new functional and animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
-const charsHidden = document.querySelectorAll('.chars-hidden');
-const charsBtn = document.querySelector('.chars-btn');
-const show = 'show';
+ExpandChars = (charsId)=>{
+  let charHidden = document.querySelectorAll(".chars-hidden")[charsId];
+  charHidden.classList.toggle("show");
+}
 
-const toggleShow = (e) => {
-  charsHidden[e.targetdataExpandChar].classList.toggle(show);
-};
-
+ExpandAccordion = (accordionId)=>{
+  let elemHidden = document.querySelectorAll(".question-body")[accordionId];
+  elemHidden.classList.toggle("show");
+  let elemBtn = document.querySelectorAll(".accordion-btn")[accordionId];
+  elemBtn.classList.toggle("show");
+}
